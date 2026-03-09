0c308fa3a1226a981c03457598ff183020ac46f0
Azeez <azeezahemedkhan@gmail.com>
2026-03-09 22:45:45 +0530

    fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,8 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(
   cors({
-    origin: [      "http://localhost:5173",
-      "https://your-frontend-url.onrender.com"],
+    origin: ["http://localhost:5173", "https://cinepeek-frontend.onrender.com"],
     credentials: true,
   }),
 );
