312b4b497447afbdbef5eef1b45121b417d40238
Azeez <azeezahemedkhan@gmail.com>
2026-03-10 00:35:31 +0530

    fix api call

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,37 +1,29 @@
-const express = require('express')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const app = express();
 
-
-app.use(express.json())
+app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://cinepeek-frontend.onrender.com"],
+    origin: ["http://localhost:5173", "https://cinepeek-8ykx.onrender.com"],
     credentials: true,
   }),
 );
-app.use(cookieParser())
+app.use(cookieParser());
 
-
-
-const authRouter = require('./routes/auth.routes')
-const userRouter = require('./routes/user.routes')
-const movieRouter = require('./routes/movie.routes')
-const TMDBRouter = require('./routes/tmdb.routes')
+const authRouter = require("./routes/auth.routes");
+const userRouter = require("./routes/user.routes");
+const movieRouter = require("./routes/movie.routes");
+const TMDBRouter = require("./routes/tmdb.routes");
 const favoriteRoutes = require("./routes/favorite.routes");
-const adminRoutes = require('./routes/admin.routes')
+const adminRoutes = require("./routes/admin.routes");
 
-
-
-app.use('/api/auth', authRouter)
-app.use('/api/users', userRouter)
-app.use('/api/movies', movieRouter)
-app.use('/api/tmdb', TMDBRouter )
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
+app.use("/api/movies", movieRouter);
+app.use("/api/tmdb", TMDBRouter);
 app.use("/api/favorites", favoriteRoutes);
-app.use("/api/admin", adminRoutes)
+app.use("/api/admin", adminRoutes);
 
-
-
-
-module.exports = app
+module.exports = app;
